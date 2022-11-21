1e04166411f6063a227a06c7d790755f4877b500
aptheparker <aptheparker@gmail.com>
2022-11-21 23:17:10 +0900

    new

--- W13/Problem#2/main.js
+++ W13/Problem#2/main.js
@@ -1,10 +1,9 @@
-const canvas = $('#canvas').get(0);
-const ctx = canvas.getContext('2d');
-
 $(document).ready(function () {
   $('#btn1').click(function () {});
   $('#btn2').click(function () {});
   $('#btn3').click(function () {
-    ctx.strokeRect(0, 0, 10, 100);
+    let canvas = document.createElement('canvas');
+    let ctx = canvas.getContext('2d');
+    $('.someBox').appendChild(ctx.strokeRect(0, 0, 100, 100));
   });
 });
