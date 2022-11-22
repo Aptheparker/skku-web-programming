d66a05167d8776d514d57d2463e341401edf194f
aptheparker <aptheparker@gmail.com>
2022-11-22 15:28:27 +0900

    changed

--- W13/Problem#2/main.js
+++ W13/Problem#2/main.js
@@ -1,26 +1,65 @@
+'use strict';
+
+function add() {
+  if ($('#targets').children().length <= 10) {
+    let element = $("<div class='unit'></div>");
+
+    $('#targets').append(element);
+  } else {
+    $('#reach').html('Reached a limit');
+    $('#btn3').blur();
+  }
+}
+
+function kill() {
+  $('#targets').children().last().remove();
+
+  $('#reach').html('');
+  $('#btn3').focus();
+}
+
+function cleanup() {
+  $('#targets').children().remove();
+  $('#reach').html('');
+  $('#btn3').focus();
+}
+
+function shape() {
+  $('#targets')
+    .children()
+    .each(function () {
+      if ($(this).css('border-radius') === '15px') {
+        $(this).css({
+          'border-radius': '0px',
+        });
+      } else {
+        $(this).css({
+          'border-radius': '15px',
+        });
+      }
+    });
+}
+
+function color() {
+  $('#targets')
+    .children()
+    .each(function () {
+      if ($(this).css('background-color') === 'red') {
+        $(this).css({
+          'background-color': 'white',
+        });
+      } else {
+        $(this).css({
+          'background-color': 'red',
+        });
+      }
+    });
+}
+
 $(document).ready(function () {
-  let someBox = document.getElementsByClassName('someBox')[0];
-  let counter = 0;
-  $('#btn1').click(function () {
-    for (let i = 0; i < someBox.length; i++) {
-      someBox.fillStyle = 'red';
-    }
-  });
-
-  $('#btn2').click(function () {});
-
-  $('#btn3').click(function () {
-    if (counter < 10) {
-      let canvas = document.createElement('canvas');
-      let ctx = canvas.getContext('2d');
-      ctx.strokeRect(50, 0, 200, 100);
-      someBox.appendChild(canvas);
-      counter++;
-    } else {
-    }
-  });
-
-  $('#btn4').click(function () {});
-
-  $('#btn5').click(function () {});
+  $('#btn1').click(color);
+  $('#btn2').click(shape);
+  $('#btn3').click(add);
+  $('#btn4').click(kill);
+  $('#btn5').click(cleanup);
 });
